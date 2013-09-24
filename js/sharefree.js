d79c06a7a72ac1b3fbf213f8ebe0fe6c3755b309
Gregor G <design@gresak.net>
2013-09-24 22:24:20 +0200

    transformed to jquery plugin

--- js/sharefree.js
+++ js/sharefree.js
@@ -19,36 +19,68 @@
  * @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
  */
 
+(function($) {
 
-
-
-var sfDialogWidth = 600
-var sfDialogHeight = 450
-
-var sfTitle = encodeURIComponent($("title").text());
-var sfImage = $("img")[0].src;
-var currentUrl= encodeURIComponent($(location).attr('href'))
-var fblink= "https://www.facebook.com/sharer.php?u=" + currentUrl + "&amp;image=" + sfImage
-var twlink = "http://twitter.com/share?text=" + sfTitle + "&amp;url=" + currentUrl
-var gplink = "https://plus.google.com/share?url=" + currentUrl
-var pilink = "http://pinterest.com/pin/create/button/?url=" + currentUrl + "&amp;media=" + sfImage + "&amp;description=" + sfTitle
-var emlink = "mailto:?subject=" + sfTitle + "&body=" + currentUrl
-$(".sf-facebook").click(function(event){
-	event.preventDefault();
-    window.open(fblink, 'fbshare', 'width=' + sfDialogWidth + ',height=' + sfDialogHeight);
-});
-$(".sf-twitter").click(function(event){
-    event.preventDefault();
-    window.open(twlink, 'twittershare', 'width=' + sfDialogWidth + ',height=' + sfDialogHeight);
-}); 
-$(".sf-gplus").click(function(event){
-    event.preventDefault();
-    window.open(gplink, 'gplusshare', 'width=' + sfDialogWidth + ',height=' + sfDialogHeight);
-}); 
-$(".sf-pinterest").click(function(event){
-    event.preventDefault();
-    window.open(pilink, 'pinshare', 'width=' + sfDialogWidth + ',height=' + sfDialogHeight);
-});
-$(".sf-email").attr("href",emlink);
-
-
+	$.fn.sharefree = function(options) {	
+		
+		var defaults = {	
+			sfDialogWidth: 600,
+			sfDialogHeight: 450,
+			template: 'smallicons',
+			templates:{
+			smallicons:'<div class="sharefree">\n\
+            	<a class="sf-facebook" href="#"><img src="img/facebook16.gif" alt="fb" class="sf-image"></a>\n\
+            	<a class="sf-twitter" href="#"><img src="img/twitter16.jpg" alt="tw" class="sf-image"></a>\n\
+            	<a class="sf-gplus" href="#"><img src="img/gplus16.jpg" alt="g+" class="sf-image"></a>\n\
+            	<a class="sf-pinterest" href="#"><img src="img/pinterest16.png" alt="p" class="sf-image"></a>\n\
+            	<a class="sf-email" href="#"><img src="img/email16.gif" alt="@" class="sf-image"></a>\n\
+        	</div>\n',
+        	bigicons:'<div class="sharefree">\n\
+	            <a class="sf-facebook" href="#"><img src="img/facebook.png" alt="fb" class="sf-image"></a>\n\
+            	<a class="sf-twitter" href="#"><img src="img/twitter.gif" alt="tw" class="sf-image"></a>\n\
+            	<a class="sf-gplus" href="#"><img src="img/gplus.jpg" alt="g+" class="sf-image"></a>\n\
+            	<a class="sf-pinterest" href="#"><img src="img/pinterest.png" alt="p" class="sf-image"></a>\n\
+            	<a class="sf-email" href="#"><img src="img/email.png" alt="@" class="sf-image"></a>\n\
+        	</div>\n'}
+		};
+		
+		var options = $.extend(defaults, options);
+		
+		var sfTitle = encodeURIComponent($("title").text());
+		var sfImage = $("img")[0].src;
+		var currentUrl= encodeURIComponent($(location).attr('href'))
+		var fblink= "https://www.facebook.com/sharer.php?u=" + currentUrl + "&amp;image=" + sfImage
+		var twlink = "http://twitter.com/share?text=" + sfTitle + "&amp;url=" + currentUrl
+		var gplink = "https://plus.google.com/share?url=" + currentUrl
+		var pilink = "http://pinterest.com/pin/create/button/?url=" + currentUrl + "&amp;media=" + sfImage + "&amp;description=" + sfTitle
+		var emlink = "mailto:?subject=" + sfTitle + "&body=" + currentUrl
+		
+		function createDOM(obj){
+				$(obj).append(options.templates[options.template]);
+		}
+		
+		return this.each(function() {
+			
+			createDOM(this);
+			
+			$(".sf-facebook").click(function(event){
+				event.preventDefault();
+			    window.open(fblink, 'fbshare', 'width=' + options.sfDialogWidth + ',height=' + options.sfDialogHeight);
+			});
+			$(".sf-twitter").click(function(event){
+			    event.preventDefault();
+			    window.open(twlink, 'twittershare', 'width=' + options.sfDialogWidth + ',height=' + options.sfDialogHeight);
+			}); 
+			$(".sf-gplus").click(function(event){
+			    event.preventDefault();
+			    window.open(gplink, 'gplusshare', 'width=' + options.sfDialogWidth + ',height=' + options.sfDialogHeight);
+			}); 
+			$(".sf-pinterest").click(function(event){
+			    event.preventDefault();
+			    window.open(pilink, 'pinshare', 'width=' + options.sfDialogWidth + ',height=' + options.sfDialogHeight);
+			});
+			$(".sf-email").attr("href",emlink);
+		});
+	}
+	
+})(jQuery)
